70774b7e54786f357fb69269aabb0e017cc5ee93
nikos <nikos.karapas86@gmail.com>
2020-12-05 14:16:32 +0200

    more stuff

--- src/app/person-form/person-form.component.ts
+++ src/app/person-form/person-form.component.ts
@@ -21,22 +21,74 @@ function emailMatcher(c: AbstractControl): { [key: string]: boolean } | null {
   styleUrls: ['./person-form.component.scss']
 })
 export class PersonFormComponent implements OnInit {
+  emailMessage: string;
+  private validationMessages={
+  required:'the field is required',
+  email:'enter a valid email'
+}
 
   constructor(private formBuilder:FormBuilder) { }
   public personForm :FormGroup;
+
   ngOnInit(): void {
     this.personForm=  this.formBuilder.group({
       fName: ['', [Validators.required, Validators.minLength(3)]],
       age: ['', [Validators.min(0)]],
+      notification: [''],
       emailGroup:this.formBuilder.group({
-        email:['',[Validators.required,Validators.email]],
-        confirmEmail:['',[Validators.required,Validators.email]]
-      }, { validator: emailMatcher })
+        email:['',[Validators.email]],
+        confirmEmail:['',[Validators.email]]
+      }, { validator: emailMatcher }),
+    
     })
+
+    this.personForm.get('notification').valueChanges.subscribe(
+      res =>{
+        console.log(res)
+        this.setNotification(res)
+      }
+    )
+
+
+    const emailControl = this.personForm.get('emailGroup.email');
+    emailControl.valueChanges.pipe(
+      // debounceTime(1000)
+    ).subscribe(
+      value => this.setMessage(emailControl)
+    );
+
   }
+  setNotification(val:string){
+    if (val == 'email'){
+      this.personForm.get('emailGroup.email').setValidators(Validators.required)
+    }
+    else{
+      this.personForm.get('emailGroup.email').clearValidators()
+    }
+    this.personForm.get('emailGroup.email').updateValueAndValidity()
+  }
+
+  makeemailRequired(){
+    this.personForm.get('emailGroup.email').setValidators(Validators.required);
+    this.personForm.get('emailGroup.email').updateValueAndValidity()
+  }
+
+
+
+  setMessage(c: AbstractControl): void {
+    this.emailMessage = '';
+    if ((c.touched || c.dirty) && c.errors) {
+      this.emailMessage = Object.keys(c.errors).map(
+        key => this.validationMessages[key]).join(' ');
+    }
+  }
+
 
   save(){
     console.log(this.personForm.value)
   }
+
+
+
 
 }
